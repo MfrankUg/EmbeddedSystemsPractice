81a5e36b78ce70bccb5ba8662dcc4809d0d5609a
MfrankUg <muhindofrank2001@gmail.com>
2026-03-12 20:48:38 +0300

    changes done to the blink code to add serial monitor functions

--- Real_Test/blink/blink.ino
+++ Real_Test/blink/blink.ino
@@ -1,15 +1,18 @@
 int ledPin = 23;
 void setup() {
   // put your setup code here, to run once:
+Serial.begin(115200);
 pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(ledPin, HIGH);
+  Serial.println("Lights ON");
   delay(1000);
   
   digitalWrite(ledPin, LOW);
+  Serial.println("Lights OFF");
   delay(1000);
 
 }
